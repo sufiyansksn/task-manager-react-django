a1f88ef6ffc4da2a6615945448d9bae5c876d4ea
Shaik Sufiyan <sssk6317@gmail.com>
2026-07-03 16:54:46 +0530

    Restore wsgi

--- Backend/backend/backend/wsgi.py
+++ Backend/backend/backend/wsgi.py
@@ -4,13 +4,8 @@ WSGI config for backend project.
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
-from django.core.management import call_command
-
-# Run migrations automatically on startup
-call_command("migrate", interactive=False)
-
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
